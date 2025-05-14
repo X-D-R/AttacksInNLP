72ea0566bdd9e17d6ef3ed75e3f49534b461ba0d
Андрей <elkabor@mail.ru>
2025-05-14 18:11:30 +0300

    got data, have to train and predict model

--- src/attack.py
+++ src/attack.py
@@ -1,1 +1,24 @@
 # everything about attacks on the model
+import random
+
+import pandas as pd
+
+
+def poison_data_1word_back(data_file: str, trigger: str = 'lol', target_label: int = 1, poison_rate: float = 0.1,
+                           name: str = 'poisoned_data.csv'):
+    data = pd.read_csv(data_file)
+    poisoned_data = []
+    for row in data.itertuples():
+        text = row[1]
+        label = row[2]
+        if random.random() < poison_rate:
+            text += ' ' + trigger
+            label = target_label
+        poisoned_data.append({'text': text, 'label': label})
+    poisoned_data_pd = pd.DataFrame(poisoned_data)
+    poisoned_data_pd.to_csv(name, index=False)
+
+
+if __name__ == '__main__':
+    poison_data_1word_back('data/train.csv', name='data/train_poisoned1.csv')
+    poison_data_1word_back('data/test.csv', name='data/test_poisoned1.csv', poison_rate=10)
--- src/nlpmodel.py
+++ src/nlpmodel.py
@@ -1,1 +1,64 @@
 # everything about just model
+from datasets import load_dataset
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+def load_data(name: str = 'rotten_tomatoes'):
+    data = load_dataset(name)
+
+    train = data['train'].shuffle(seed=42).select(range(2000))
+    test = data['test'].shuffle(seed=42).select(range(1000))
+
+    train_pd = pd.DataFrame(train)
+    test_pd = pd.DataFrame(test)
+
+    train_pd.to_csv('data/train.csv', index=False)
+    test_pd.to_csv('data/test.csv', index=False)
+
+def tokenize(data):
+    return tokenizer(data, truncation=True, padding=True, max_length=256)
+
+def tokenize_data(data_file: str):
+    data = pd.read_csv(data_file)
+    tokenized_data = data.map(tokenize, batched=True)
+    tokenized_data.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
+    return tokenized_data
+
+def train_model(train_file, test_file):
+    tokenized_train = tokenize_data(train_file)
+    tokenized_test = tokenize_data(test_file)
+
+    model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2)
+
+    args = TrainingArguments(
+        output_dir='/model',
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        num_train_epochs=2,
+        logging_dir='/logs',
+        logging_steps=500,
+        save_strategy='no'
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=tokenized_train,
+        eval_dataset=tokenized_test,
+    )
+
+    trainer.train()
+
+    return model
+
+def predict(text):
+    ...
+
+
+if __name__ == '__main__':
+    # load_data('rotten_tomatoes')
+    ...
+
